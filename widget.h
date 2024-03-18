32bc7597f58c56092a14e416da99ca64510baff3
YYun-D <yeo1998@naver.com>
2024-03-18 11:49:32 +0900

    Add widget.h

---
+++ widget.h
@@ -0,0 +1,44 @@
+#ifndef WIDGET_H
+#define WIDGET_H
+
+#include <QMainWindow>
+
+QT_BEGIN_NAMESPACE
+namespace Ui {
+class MainWindow;
+}
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+    int money{0};
+    void changeMoney(int diff);
+
+private slots:
+    void on_pushButton_clicked();
+
+    void on_lcdNumber_overflow();
+
+    void on_pushButton_2_clicked();
+
+    void on_pushButton_3_clicked();
+
+    void on_pushButton_4_clicked();
+
+    void on_pushButton_5_clicked();
+
+    void on_pushButton_6_clicked();
+
+    void on_pushButton_7_clicked();
+
+    void on_pushButton_8_clicked();
+
+private:
+    Ui::MainWindow *ui;
+};
+#endif // WIDGET_H
